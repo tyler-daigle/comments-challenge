e31f5038de510cba25addfd3de8d4ba11be165fb
Tyler D <drelyt@gmail.com>
2022-01-03 15:18:27 -0500

    More comment styling

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, Fragment } from "react";
+import { useState, useEffect } from "react";
 import VoteButton from "./VoteButton";
 import styles from "../style/Comment.module.css";
 import avatarPlaceHolder from "../images/avatars/image-amyrobson.png";
@@ -11,8 +11,14 @@ export default function Comment({ comment }) {
   }, []);
 
   const incVotes = () => setNumVotes(numVotes + 1);
-  const decVotes = () => setNumVotes(numVotes - 1);
+  const decVotes = () => {
+    if (numVotes > 0) {
+      setNumVotes(numVotes - 1);
+    }
+  };
 
+  // TODO: do check for current user and set the "you" badge and the edit and delete buttons
+  // TODO: extract the commentHeader and commentControls to separate components.
   return (
     <div className={styles.commentContainer}>
       <div className={styles.commentHeader}>
@@ -28,7 +34,7 @@ export default function Comment({ comment }) {
           decVotes={decVotes}
         />
         <button type="button" className={styles.replyButton}>
-          Reply
+          ↩️ Reply
         </button>
       </div>
     </div>
